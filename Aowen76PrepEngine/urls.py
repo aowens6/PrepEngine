1fa3a51a78bac19619f93b532dfdae7250ad01a7
aowens6 <austyn.owens1@gmail.com>
2019-04-30 18:14:42 -0500

    Splash page is set to login, also runtime.txt

--- Aowen76PrepEngine/urls.py
+++ Aowen76PrepEngine/urls.py
@@ -21,11 +21,11 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('home/', include('home.urls')),
     path('tutorSet/', include('tutor_sets.urls')),
     path('creator/', include('users.urls')),
     path('reports/', home_views.ReportListView.as_view(), name='reports'),
     path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
