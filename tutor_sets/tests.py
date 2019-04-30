30328d7e8714b80041ea49c3a2000cb6a277743f
aowens6 <austyn.owens1@gmail.com>
2019-04-30 14:04:21 -0500

    Test example, requirements.txt, .gitignore

--- tutor_sets/tests.py
+++ tutor_sets/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models import TutorSet
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your tests here.
+
+class CreateTutorSet(TestCase):
+
+    def create_tutor_set(self):
+        User.objects.create_user(username='default',email=None,password=None)
+        return TutorSet.objects.create(
+            title="Test Set from test.py",
+            description='Description for test.py Tutor Set',
+            author=User.objects.first(),
+            date_created=timezone.now(),
+            date_updated=timezone.now()
+            )
+
+    def test_tutor_set_creation(self):
+        tutorSet = self.create_tutor_set()
+        self.assertTrue(isinstance(tutorSet, TutorSet))
+        self.assertEqual(tutorSet.__str__(), tutorSet.title)
