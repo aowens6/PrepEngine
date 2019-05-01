8c470a1cfa8d7dc3aa40e8e5f4c80a1ba147bb78
aowens6 <austyn.owens1@gmail.com>
2019-04-30 19:40:41 -0500

    Settings for environment variables

--- Aowen76PrepEngine/settings.py
+++ Aowen76PrepEngine/settings.py
@@ -21,9 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(ih3$viwijza4+rmw6y71ipulshl)lhop9b4gvoia39hjx24qe'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
